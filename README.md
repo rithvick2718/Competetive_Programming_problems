5ff135a7acae5a761eed5f485d3dbbb61a8c4dc8
rithvick2718 <sairithvickgofficial@gmail.com>
2024-04-08 00:55:07 +0530

    this is annoying
    well I understand it is good to type a message every time you change sometimes it is .... annoying

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ I am still new to this but I seem to get the hang of this now
 I have added geek_for_geeks file, the problems I have done in DSA Course
 
 And udemy is the main file I am working on right now
+
+Wooh i love coding
--- udemy/S1/test.cpp
+++ udemy/S1/test.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-#include <cstdio>
-
+#include<bits/stdc++.h>
+#include<typeinfo>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    //this is to test a few differences between ponters and references
-    int a =6;
-    int& c= a;
-    int* d =&a;
-    c=9;
-    cout<<*d;
+    int arr[]= {11,22,33,44,55,66,77,88,99};
+    bool p = binary_search(arr, arr+8, 23);
+    if (binary_search(arr, arr+8, 23)) cout<<"yes bro";
+    else cout<<"nope man nope";
     return 0;
 }
