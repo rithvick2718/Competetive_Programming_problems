d7e5ab295712713e4778002b66800d7ee39ee943
rithvick2718 <sairithvickgofficial@gmail.com>
2024-04-12 15:19:41 +0530

    Solved a Question
    Solved Udemy Problem Set 1 Q: Game

---
+++ udemy/S2/984_Game.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    if(n%2==1) cout<<a[(n-1)/2];
+    else cout<<a[(n/2)-1];
+}
