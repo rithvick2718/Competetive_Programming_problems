3cebaaa1b6fcdb70ed1bcdf61e5f4e38d770fa78
rithvick2718 <sairithvickgofficial@gmail.com>
2024-04-13 21:56:50 +0530

    Japan IOI
    Well solved 1 Japan Olympiad in Informatics

---
+++ udemy/S2/Stove.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N,K;
+    cin>>N;
+    cin>>K; //the idea is simple
+    if(K>=N)
+    {
+        cout<<N;
+        return(0);
+    }
+    else
+    {
+        vector<int> arr;
+        int a,temp;
+        cin>>a;
+        temp =a;
+        for(int i=0;i<N-1;i++)
+        {
+            cin>>a;
+            arr.push_back(a-temp-1);
+            temp=a;
+        }
+        sort(arr.begin(),arr.end());
+        int s=N;
+        for(int i=0; i<N-K; i++)
+        {
+            s=s+arr[i];
+        }
+        cout<<s;
+    }
+    return(0);
+}
