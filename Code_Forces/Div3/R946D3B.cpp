85c15406585851eeb9f57dd21663ca71f0f13bf9
rithvick2718 <sairithvickgofficial@gmail.com>
2024-05-20 22:16:43 +0530

    solve round 946 d3 B

---
+++ Code_Forces/Div3/R946D3B.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int n;
+    while(t--)
+    {
+        cin>>n;
+        string s,r;
+        char y;
+        for (size_t i = 0; i < n; i++)
+        {
+            cin>>y;
+            s.push_back(y);
+        }
+        
+        r=s;
+        sort(r.begin(),r.end());
+        auto unique_end = std::unique(r.begin(), r.end());
+        r.erase(unique_end, r.end());
+        map<char,char> k;
+        int z =(int) r.size();
+        for (size_t i = 0; i < z; i++)
+        {
+            k[(char)r[i]]=(char)r[z-1-i];
+        }
+        z=(int)s.size();
+        for(int i =0; i< z; i++)
+        {
+            s[i]=k[s[i]];
+        }
+        cout<<s<<"\n";
+    }
+}
