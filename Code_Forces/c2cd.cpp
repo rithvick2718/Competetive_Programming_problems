d94e2e38792ed5abc91e20926e7b328017a2c565
rithvick2718 <sairithvickgofficial@gmail.com>
2024-05-22 16:06:47 +0530

    This is the solution to https://codeforces.com/contest/1768/problem/A

---
+++ Code_Forces/c2cd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    int k;
+    while(t--)
+    {
+        cin>>k;
+        cout<<k-1<<"\n";
+    }
+}
