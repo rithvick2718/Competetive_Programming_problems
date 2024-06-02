be2e1d8dae12e6f0eb7401271c50b1e76b195de6
rithvick2718 <sairithvickgofficial@gmail.com>
2024-06-02 10:59:46 +0530

    used test cpp for 1973B

--- Own_Questions/test.cpp
+++ Own_Questions/test.cpp
@@ -1,20 +1,77 @@
-#include<bits/stdc++.h>
-using namespace std;
-int pow(int a, int n)
-{
-    if(n==0) return(1);
-    if(n%2==0) return(pow(a,n/2)*pow(a,n/2));
-    else return(pow(a,(n-1)/2)*pow(a,(n-1)/2)*a);
-}
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    vector<int> arr;
-    for ( int i = 0; i < 10; i++)
-    {
-        arr.push_back(pow(2,i));
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    vector<unsigned int> arr;
+     
+    inline int countBits(unsigned int num) {
+        return num == 0 ? 1 : 32 - __builtin_clz(num);
     }
-    auto k =upper_bound(arr.begin(), arr.end(), 64);
-    cout<<*k;
-}
+     
+    inline bool getKthBit(unsigned int k, unsigned int num) {
+        return (num >> (k - 1)) & 1;
+    }
+     
+    int maxConsecutiveZeros(int bitPos) {
+        int maxZeros = 0;
+        int currentZeros = 0;
+     
+        for (unsigned int num : arr) {
+            if (getKthBit(bitPos, num)) {
+                if (currentZeros > maxZeros) {
+                    maxZeros = currentZeros;
+                }
+                currentZeros = 0;
+            } else {
+                currentZeros++;
+            }
+        }
+     
+        if (currentZeros > maxZeros) {
+            maxZeros = currentZeros;
+        }
+        return maxZeros;
+    }
+     
+    int findLonelyBit(int maxBits) {
+        int maxZeros = 0;
+     
+        for (int i = 1; i <= maxBits; ++i) {
+            int zeros = maxConsecutiveZeros(i);
+            if (zeros != static_cast<int>(arr.size())) {
+                if (zeros > maxZeros) {
+                    maxZeros = zeros;
+                }
+            }
+        }
+     
+        return maxZeros + 1;
+    }
+     
+    int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+     
+        int t;
+        cin >> t;
+     
+        while (t--) {
+            arr.clear();
+     
+            int n;
+            cin >> n;
+            unsigned int maxNum = 0;
+            arr.resize(n);
+     
+            for (int i = 0; i < n; ++i) {
+                cin >> arr[i];
+                if (arr[i] > maxNum) {
+                    maxNum = arr[i];
+                }
+            }
+     
+            int maxBits = countBits(maxNum);
+            cout << findLonelyBit(maxBits) << "\n";
+        }
+     
+        return 0;
+    }
