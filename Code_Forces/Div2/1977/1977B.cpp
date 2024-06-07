9803e93b05b5a1cdfac9a03c01786a71a852a6a2
Rithvick <sairithvickgofficial@gmail.com>
2024-06-07 07:38:35 +0530

    Solved 1977B.cpp

--- Code_Forces/Div2/1977/1977B.cpp
+++ Code_Forces/Div2/1977/1977B.cpp
@@ -1,27 +1,48 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
-vector<int> a;
+void solve (int &x) //taking by reference, that's my boy
+{
+    vector<int> booles; //need to dynammically allocate, by since array won't be larger than 32 I dont see this as a performance issue. Yet.
+    while(x!=0) //do it till our boy x is 0
+    {
+        if(x&1) //so we have a 1 
+        {
+            if((x&2)==2) //checks for 11
+            {
+                booles.push_back(-1); // so I add 1 and sub -1
+                x=(x+1)>>1; // see the above comment
+            }
+            else
+            {
+                booles.push_back(1); //yeah my boy is fine
+                x=x>>1;
+            }
+        }
+        else //this one is fine
+        {
+            booles.push_back(0);
+            x=x>>1;
+        }
+    }
+    int m = booles.size();
+    cout<<m<<"\n";
+    for(int b : booles)
+    {
+        cout<<b<<" ";
+    }
+    cout<<"\n";
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     cin>>t;
-    unsigned int x;
+    int x;
     while (t--)
     {
         cin>>x;
-        while(x!=0)
-        {
-            a.push_back(x%2);
-            x=x/2;
-        }
-        cout<<a.size()<<"\n";
-        for(auto k : a)
-        {
-            cout<<k<<" ";
-        }
-        cout<<"\n";
-        a.clear();
+        solve(x);
     }
 }
