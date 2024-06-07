220e7863d973ef67f9258f668fc26702089a531e
Rithvick <sairithvickgofficial@gmail.com>
2024-06-07 07:46:16 +0530

    solved 1957C

--- Code_Forces/Div1 + Div2/1975/1975C.cpp
+++ Code_Forces/Div1 + Div2/1975/1975C.cpp
@@ -1,38 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> arr;//array
-int maxmed(int index)
-{
-    int n = arr.size();
-    if (index == 0) {
-        return (arr[index] + arr[index + 1]) / 2;
-    } else if (index == n - 1) {
-        return (arr[index] + arr[index - 1]) / 2;
-    } else {
-        return max({arr[index], (arr[index] + arr[index + 1]) / 2, (arr[index] + arr[index - 1]) / 2});
-    }
-}
-int main()
-{
-    int t; //testcases
-    cin>>t;
-    int n; //array size
-    int h; //helping var
-    while(t--) //code for each test case
+int solve(vector<int> a) {
+    int size =a.size();
+    if(size == 2) return(min(a[0],a[1]));
+    int Max=-1;
+    for(int i =0; i<a.size()-2; i++)
     {
-        cin>>n;
-        int m=n;
-        while(m--)
-        {
-            cin>>h;
-            arr.push_back(h);
-        }//getting the array the largest element and it's index
-        int max =std::numeric_limits<int>::min();
-        for(int i=0;i<arr.size();i++)
-        {
-            if(max<maxmed(i)) max= maxmed(i);
-        }
-        cout<<max<<"\n";
-        arr.clear();
+        vector<int> t = {a[i],a[i+1],a[i+2]};
+        sort(t.begin(),t.end());
+        Max=max(Max, t[1]);
     }
+    return(Max);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        cout<<solve(a)<<"\n";
+    }
+    return 0;
 }
