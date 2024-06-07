7f3931fd6084ce1382c20bb8ef69365c682b9127
Rithvick <sairithvickgofficial@gmail.com>
2024-06-07 07:56:22 +0530

    Current inefficient solution for 1974C

--- Code_Forces/Div3/1974/1974C.cpp
+++ Code_Forces/Div3/1974/1974C.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> arr; //global array
-bool compare (int i, int j)
-{
-    int c=0;
-    if(arr[i]==arr[j]) c++;
-    if(arr[i+1]==arr[j+1]) c++;
-    if(arr[i+2]==arr[j+2]) c++;
-    return(c==2);
+inline bool compare(const vector<int> &arr, int i, int j) {
+    // Compare the three positions
+    bool first = (arr[i] == arr[j]);
+    bool second = (arr[i + 1] == arr[j + 1]);
+    bool third = (arr[i + 2] == arr[j + 2]);
+
+    // Return true if exactly two out of the three are true
+    return (first + second + third) == 2;
 }//dont think this can be faster
 int main()
 {
@@ -19,21 +19,19 @@ int main()
     while(t--)
     {
         cin>>n;
-        int h;
+        vector<int> arr(n);
         for (size_t i = 0; i < n; i++)
         {
-            cin>>h;
-            arr.push_back(h);
+            cin>>arr[i];
         }//getting the array
         int s=0;
-        for(int i=0; i<n-2; i++)
+        for(int i=0; i<n-3; i++)
         {
             for(int j =i+1; j<n-2; j++)
             {
-                if(compare(i,j)) s++;
+                if(compare(arr,i,j)) s++;
             }
         }
         cout<<s<<"\n";
-        arr.clear(); //clearing array for the next use
     }
 }
