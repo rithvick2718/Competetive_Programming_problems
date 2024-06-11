68ad93ffc9bb11ae46afa7f8923df0cdb15c3f76
Rithvick <sairithvickgofficial@gmail.com>
2024-06-11 22:55:43 +0530

    Solved 1985B

---
+++ Code_Forces/Div4/1985B.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==3) cout<<3<<"\n";
+        else cout<<2<<"\n";
+    }
+}
