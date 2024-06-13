c61a5856afd874a468e4d2fe660594f1f7327952
Rithvick <sairithvickgofficial@gmail.com>
2024-06-13 12:30:48 +0530

    a small update

--- Code_Forces/Div1/misc/1967A.cpp
+++ Code_Forces/Div1/misc/1967A.cpp
@@ -10,29 +10,29 @@ int main()
     {
         int n; //this is guarrented to be int type
         cin>>n; //take this
-        long long int k; //10^12 so long long int
+        long long int k; //<=10^12 so long long int
         cin>>k;
-        vector<long long int> a(n); //all are 10^12 so long long int
+        vector<long long int> a(n); //all are <=10^12 so long long int
         for (int i = 0; i < n; i++)
         {
             cin>>a[i];
         } //get the array
         sort(a.begin(),a.end()); //sort it
-        auto it = upper_bound(a.begin(),a.end(),a[0]);
+        long long int s =a[0];
+        auto it = upper_bound(a.begin(),a.end(),s);
         int toadd=(int)((it - a.begin())); //this has the number of elements equal at the start
-        long long int diff = *it-a[0];
+        long long int diff = *it-s;
         while(k>=toadd*diff && toadd!=n)
         {
-            a[0]=*it;
+            s=*it;
             k=k-toadd*diff;
-            it = upper_bound(a.begin(),a.end(),a[0]);
+            it = upper_bound(a.begin(),a.end(),s);
             toadd=(int)((it - a.begin()));
-            diff = *it-a[0];
+            diff = *it-s;
         }
-        //now either k< toadd*diff or toadd==n so that means
-        //lets tackle it == a.end()
-        a[0]=a[0]+k/toadd;
+        //now either k< toadd*diff or toadd==n so to tackle that this is sufficient
+        s=s+k/toadd;
         k=k%toadd;
-        cout<<((long long int)n)*(a[0])+((long long int)(k-toadd+1))<<"\n";
+        cout<<((long long int)n)*(s)+((long long int)(k-toadd+1))<<"\n";
     }
 }
