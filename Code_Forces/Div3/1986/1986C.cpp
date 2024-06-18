544580350556de87990f6ada1a4678fecd1a453c
Rithvick <sairithvickgofficial@gmail.com>
2024-06-18 22:15:25 +0530

    I am preparing myself for Div3 a small step

---
+++ Code_Forces/Div3/1986/1986C.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; //testcases
+    cin>>t;
+    while(t--)
+    {
+        //code for each testcase
+    }
+}
