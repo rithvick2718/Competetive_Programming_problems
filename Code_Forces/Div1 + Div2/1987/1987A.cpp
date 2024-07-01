b1c01734de3514727bf11e692fe2668a38b37126
Rithvick <sairithvickgofficial@gmail.com>
2024-07-01 18:47:39 +0530

    Solved 1987A

---
+++ Code_Forces/Div1 + Div2/1987/1987A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    int n,k;
+    while(t--)
+    {
+        //code
+        cin>>n>>k;
+        cout<<(n-1)*k+1<<"\n";
+    }
+}
