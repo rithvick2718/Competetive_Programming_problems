7e5c8183f2c964d3012fa7880ccacd6ebd1c4eb5
Rithvick <sairithvickgofficial@gmail.com>
2024-07-02 00:10:17 +0530

    Solved 1987C

--- Code_Forces/Div1 + Div2/1987/1987C.cpp
+++ Code_Forces/Div1 + Div2/1987/1987C.cpp
@@ -1,42 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void nsecondoperation(vector<int> &h, int n)
-{
-    int size =h.size();
-    while(n--)
-    {
-        for(int i=0; i<size-1; i++)
-        {
-            if(h[i]>h[i+1]) h[i]=max(0,h[i]-1);
-        }
-        h[size-1]=max(0,h[size-1]-1);
-    }
-}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> h(n);
-        for (int i = 0; i < n; i++) {
-            cin >> h[i];
-        }
-        int seconds = 0;
-        auto max_it = std::max_element(h.begin(), h.end());
-        int max_value = *max_it;
-        while(max_value!=0)
+    int t; //testcases
+    cin>>t;
+    int n;
+    while(t--)
+    {
+        cin>>n;
+        int h[n];
+        for(int i=0; i<n; i++)
         {
-            seconds+=max_value;
-            nsecondoperation(h,max_value);
-            max_it = std::max_element(h.begin(), h.end());
-            max_value = *max_it;
-        }      
-        cout << seconds << "\n";
+            cin>>h[i];
+        }
+        int dp = h[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+            dp=max(dp+1,h[i]);
+        }
+        cout<<dp<<"\n";
     }
-    
-    return 0;
 }
