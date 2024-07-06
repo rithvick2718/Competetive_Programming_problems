6346145d07c7ca9f5a494bc63d4bc2c4777bbd0a
Rithvick <sairithvickgofficial@gmail.com>
2024-07-07 00:22:12 +0530

    Solved 1933C

--- Code_Forces/Div3/misc/1933C.cpp
+++ Code_Forces/Div3/misc/1933C.cpp
@@ -1,47 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Computes the greatest common divisor of a and b
-int gcd(int a, int b) {
-    int r = a % b;
-    while (r) {
-        a = b;
-        b = r;
-        r = a % b;
-    }
-    return b;
-}
-
-// Computes the power of n in m
-int maxPower(int m, const int &n) {
-    int ans = 0;
-    if (n == 1) return 0;
-    while (m % n == 0) {
-        ans++;
-        m = m / n;
-    }
-    return ans;
-}
-// The main solve function
-int solve(int a, int b, int l) {
-    if (a == b)
-        return (maxPower(l, a)+1);
-    int g = gcd(a, b); // We know a != b
-    // if (g == 1){
-    //     int X = maxPower(l, a);
-    //     int Y = maxPower(l, b);
-    //     return ((X + 1) * (Y + 1));
-    // }
-    int ans=0;
-    int copy =l*a;
-    int X =maxPower(l,a);
-    for(int x=0;x<=X;x++)
+int solve(int &a, int &b, int & l)
+{
+    set<int> ans;
+    ans.insert(l);
+    int copy =l;
+    while(copy%b==0)
     {
-        copy=copy/a;
-        ans= ans+1+maxPower(copy,b);
+        copy/=b;
+        ans.insert(copy);
     }
-    return(ans);
+    while(l%a==0)
+    {
+        l/=a;
+        ans.insert(l);
+        copy =l;
+        while(copy%b==0)
+        {
+            copy/=b;
+            ans.insert(copy);
+        }
+    }
+    return ans.size();
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -50,7 +31,7 @@ int main() {
     int a, b, l; // The parameters in question 
     while (t--) {
         cin >> a >> b >> l; // a and b are guaranteed to be >2
-        cout << solve(a, b, l) << "\n";
+        cout << solve(a,b,l) << "\n";
     }
     return 0; // Added return statement for main
 }
