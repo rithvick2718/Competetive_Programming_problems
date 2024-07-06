d2d9b8c7c0c248a074f35af8f060163227053143
Rithvick <sairithvickgofficial@gmail.com>
2024-07-06 16:30:10 +0530

    Solved 1989B

--- Code_Forces/Educational Div2/1989/1989B.cpp
+++ Code_Forces/Educational Div2/1989/1989B.cpp
@@ -1,46 +1,30 @@
 #include<bits/stdc++.h>
 #include<string>
 using namespace std;
-int longestCommonSubsequence(string& text1, string& text2)
+int maximize(string &a, string &b)
 {
-    int n = text1.size();
-    int m = text2.size();
-
-    // initializing 2 vectors of size m
-    vector<int> prev(m + 1, 0), cur(m + 1, 0);
-
-    for (int idx2 = 0; idx2 < m + 1; idx2++)
-        cur[idx2] = 0;
-
-    for (int idx1 = 1; idx1 < n + 1; idx1++) {
-        for (int idx2 = 1; idx2 < m + 1; idx2++) {
-            // if matching
-            if (text1[idx1 - 1] == text2[idx2 - 1])
-                cur[idx2] = 1 + prev[idx2 - 1];
-
-            // not matching
-            else
-                cur[idx2]
-                    = 0 + max(cur[idx2 - 1], prev[idx2]);
+    int ans =0;
+    for(int j=0; j<b.length(); j++)
+    {
+        int ptr =j;
+        for(int i=0; i<a.length();i++)
+        {
+            if(a[i]==b[ptr]) ptr++;
+            if(ptr==b.length()) break;            
         }
-        prev = cur;
+        ans = max(ans, ptr -j);
     }
-
-    return cur[m];
+    return ans;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int t;
     cin >> t;
     while (t--) {
         string a, b;
         cin >> a >> b;
-        int lena = a.length();
-        int lenb = b.length();
-        int ans=lena+lenb-longestCommonSubsequence(a,b);
-        cout<<ans<<"\n";
+        cout<<a.length()+b.length()-maximize(a,b)<<"\n";
     }
     return 0;
 }
