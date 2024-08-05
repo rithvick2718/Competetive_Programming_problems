968f212aa6ec86de15b6139c24a633e3078e9c9e
Rithvick <sairithvickgofficial@gmail.com>
2024-08-05 23:01:12 +0530

    Idea in comments

---
+++ CSES/Introductory Problems/TwoKnights.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    cout<<0<<"\n";
+    for(int i=2;i<=n;i++)
+    {
+        //sup my boi use recursion
+        // see whatever is the answer of the last times 4 + 4 times the ways where knight is in (k-1)*(k-1) and your ways
+    }
+}
