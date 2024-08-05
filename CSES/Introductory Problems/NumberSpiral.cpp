eb560954acc9589872f50ed2e34aa97fcfc76c03
Rithvick <sairithvickgofficial@gmail.com>
2024-08-05 10:35:13 +0530

    Solved Number Spiral

--- CSES/Introductory Problems/NumberSpiral.cpp
+++ CSES/Introductory Problems/NumberSpiral.cpp
@@ -6,9 +6,35 @@ int main()
     cin.tie(NULL);
     int t;
     cin>>t;
-    int r,c;
+    unsigned long long r,c;
     while(t--)
     {
-        
+        cin>>c>>r;
+        if(r>c)
+        {
+            if(r&1)
+            {
+                cout<<r*r-c+1<<"\n";
+            }
+            else
+            {
+                cout<<r*r-2*r+2+c-1<<"\n";
+            }
+        }
+        else if(r<c)
+        {
+            if(c&1)
+            {
+                cout<<c*c-2*c+2+r-1<<"\n";
+            }
+            else
+            {
+                cout<<c*c-r+1<<"\n";
+            }
+        }
+        else
+        {
+            cout<<c*c-c+1<<"\n";
+        }
     }
 }
