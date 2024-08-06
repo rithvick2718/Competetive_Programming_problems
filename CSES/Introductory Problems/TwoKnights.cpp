91aba7b606c27bacb669c83be117f509f0c42c95
Rithvick <sairithvickgofficial@gmail.com>
2024-08-06 18:15:32 +0530

    Solved two Knights

--- CSES/Introductory Problems/TwoKnights.cpp
+++ CSES/Introductory Problems/TwoKnights.cpp
@@ -4,12 +4,31 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
+    long long n;
     cin>>n;
-    cout<<0<<"\n";
-    for(int i=2;i<=n;i++)
+    long long t[] ={0,6,28,96,252,550,1056,1848}; // so these are the first 8 values
+    if(n<=8)
     {
-        //sup my boi use recursion
-        // see whatever is the answer of the last times 4 + 4 times the ways where knight is in (k-1)*(k-1) and your ways
+        for(long long i=0; i<n;i++)
+        {
+            cout<<t[i]<<"\n";
+        }
+    }
+    else
+    {
+        for(long long i=0; i<8;i++)
+        {
+            cout<<t[i]<<"\n";
+        }
+        long long l1=t[7];
+        long long l2=t[6];
+        long long h;
+        for(long long k=9;k<=n;k++)
+        {
+            h=l2+4*k*k*k-12*k*k-2*k+34;
+            cout<<h<<"\n";
+            l2=l1;
+            l1=h;
+        }
     }
 }
