d3db13b64cce74abb4f5b50d5be5a5bc713757da
Rithvick <sairithvickgofficial@gmail.com>
2024-08-08 20:06:09 +0530

    Solved 1999F
    There have been multiple optimization to bring it to 124ms

--- Code_Forces/Div4/1999/1999F.cpp
+++ Code_Forces/Div4/1999/1999F.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-# define mod 1000000007
+# define mod (long long) 1000000007
 using namespace std;
 long long gcd(long long a, long long b)//fine
 {
@@ -13,6 +13,19 @@ long long gcd(long long a, long long b)//fine
         r=a%b;
     }
     return b;
+}
+long long inv (long long t) //debugged
+{
+    long long p = (long long) mod -2;
+    long long ans=1;
+    t=t%mod;
+    while(p)
+    {
+        if(p&1) ans = (ans*t)%mod;
+        p>>=1;
+        t=(t*t)%mod;
+    }
+    return ans%mod;
 }
 long long nCr(long long n, long long r) {
     if (r > n) return 0;
@@ -29,8 +42,8 @@ long long nCr(long long n, long long r) {
         g=gcd(m,d);
         m/=g;
         d/=g;
-        res/=d;
-        res*=m;
+        res=(res*m)%mod;
+        res=(res*inv(d))%mod;
     }
     return res;
 }
@@ -40,11 +53,19 @@ long long solve(int n, int k, int cnt)
     long long K = (long long) k;
     long long CNT = (long long) cnt;
     long long ans=0;
-    for(long long i=max(K+CNT-N,(long long) 0); i<= min(CNT,(K-1)/2) ; i++)
+    long long lower =max(K+CNT-N,(long long) 0);
+    long long upper =min(CNT,(K-1)/2);
+    long long nCr1=nCr(CNT,lower)%mod;
+    long long nCr2=nCr(N-CNT,K-lower)%mod;
+    for(long long i=lower; i<= upper; i++)
     {
-        ans=(ans+((nCr(CNT,i)%mod)*(nCr(N-CNT,K-i)%mod))%mod);
+        ans=(ans+(nCr1*nCr2)%mod)%mod;
+        nCr1=(nCr1*(CNT-i))%mod;
+        nCr1=(nCr1*inv(i+1))%mod;
+        nCr2=(nCr2*(K-i))%mod;
+        nCr2=(nCr2*inv(N-CNT+i-K+1))%mod;
     }
-    return ans;
+    return ans%mod;
 }
 int main()
 {
