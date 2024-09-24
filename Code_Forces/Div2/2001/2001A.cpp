f5234e294cf8cd8274952192182b10b669e24f0f
Rithvick <sairithvickgofficial@gmail.com>
2024-09-24 13:20:59 +0530

    Solved 2001A

--- Code_Forces/Div2/2001/2001A.cpp
+++ Code_Forces/Div2/2001/2001A.cpp
@@ -11,19 +11,13 @@ int main()
     {
         //code
         cin>>n;
-        map<int,int> m;
         int a;
+        int b[n]={};
         for(int i=0; i<n; i++)
         {
             cin>>a;
-            if(m.find(a)==m.end()) m[a]=1;
-            else m[a]++;
+            b[a-1]++;
         }
-        int max=-1;
-        for(auto x : m)
-        {
-            if(x.second > max) max=x.second;
-        }
-        cout<<n-max<<"\n";
+        cout<<n-*max_element(b,b+n)<<"\n";
     }
 }
