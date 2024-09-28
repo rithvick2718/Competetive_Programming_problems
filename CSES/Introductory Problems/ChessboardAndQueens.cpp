dab1e9554ab36bbe42df824783db747518459268
Rithvick <sairithvickgofficial@gmail.com>
2024-09-28 10:29:23 +0530

    DID NOT SOLVE

--- CSES/Introductory Problems/ChessboardAndQueens.cpp
+++ CSES/Introductory Problems/ChessboardAndQueens.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout<<rand()%1000;
-    // bool occ[8][8];
-    // char h;
-    // for(int i=0; i<8; i++){
-    //     for(int j =0 ;j<8; j++){
-    //         cin>>h;
-    //         if(h=='.') occ[i][j]=true;
-    //         else occ[i][j]=false;
-    //     }
-    // }
-    
+    // bool a[8][8]={};
+    vector<pair<int,int>> p;
+    char c;
+    for(int i=0; i<8; i++)
+    {
+        for(int j=0; j<8; j++)
+        {
+            cin>>c;
+            if(c=='*') p.push_back({i+1,j+1});
+        }
+    }
+    for(auto x : p)
+    {
+        cout<<x.first<<" "<<x.second<<"\n";
+    }
 }
