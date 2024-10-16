4fa0aac6899bda7b5afdee23596e1f7ea00ee111
Rithvick <sairithvickgofficial@gmail.com>
2024-10-16 11:29:56 +0530

    THESE ARE DP PROBLEMS! (NOT SOLVED)
    COMEBACK WHEN YOU ARE READYgit status

---
+++ Code_Forces/Div2/2022/2022C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //code
+        cin>>n;
+        bool a1[n]={};
+        bool a2[n]={};
+        string r1,r2;
+        cin>>r1>>r2;
+        //THIS IS DP COMEBACK WHEN YOU ARE READY!!
+    }
+}
---
+++ Code_Forces/Educational Div2/2025/2025D.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,m;
+    cin>>n,m; //so I know that I can have at max m points
+    // we cannot transfer points between n,m
+    int x=0;
+    int r;
+    for(int i=0; i<n; i++)
+    {
+        cin>>r;
+    }
+    //THIS IS DP COMEBACK WHEN YOU ARE READY!!
+}
