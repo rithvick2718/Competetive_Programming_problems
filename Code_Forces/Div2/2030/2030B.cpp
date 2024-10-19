2961a688df581c72a5ea4ba37d1343ca7e04de23
Rithvick <sairithvickgofficial@gmail.com>
2024-10-19 23:48:30 +0530

    Solved 2030B

---
+++ Code_Forces/Div2/2030/2030B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //code
+        cin>>n;
+        for(int i=0; i<n-1; i++)
+        {
+            cout<<0;
+        }
+        cout<<1<<"\n";
+    }
+}
