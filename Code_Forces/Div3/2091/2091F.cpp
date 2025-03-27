c7b9d2e6164dff243ad32fc1b5b67c5ff1ac8a24
Rithvick <sairithvickgofficial@gmail.com>
2025-03-27 23:21:41 +0530

    DP unsolved

---
+++ Code_Forces/Div3/2091/2091F.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 998244353
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        //code
+        // this is DP
+    }
+}
