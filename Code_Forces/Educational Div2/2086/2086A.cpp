09b4c3639f47a57d2b90b07f62c21452c928afdf
Rithvick <sairithvickgofficial@gmail.com>
2025-04-03 22:28:37 +0530

    Solved 2086A

---
+++ Code_Forces/Educational Div2/2086/2086A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    int tt,n;
+    cin>>tt;
+    while(tt--)
+    {
+        //code
+        cin>>n;
+        cout<<2*n<<"\n";
+    }
+}
