acc29b39df7e24b7b744c3110bc7aa947d53133d
Rithvick <sairithvickgofficial@gmail.com>
2025-04-09 00:30:45 +0530

    Solved 2093A

---
+++ Code_Forces/Div3/2093/2093A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt,k;
+    cin>>tt;
+    while(tt--)
+    {
+        //code
+        cin>>k;
+        if(k&1) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
