a6c43cbd90fc48b50bd8841e41ef87552ed14136
Rithvick <sairithvickgofficial@gmail.com>
2025-04-13 23:32:52 +0530

    Solved 2094A

---
+++ Code_Forces/Div4/2094/2094A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin>>tt;
+    string i,j,k;
+    while(tt--)
+    {
+        cin>>i>>j>>k;
+        cout<<i[0]<<j[0]<<k[0]<<"\n";
+    }
+}
