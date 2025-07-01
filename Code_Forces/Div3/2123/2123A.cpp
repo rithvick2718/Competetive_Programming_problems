52bea57fed19e978907a24c8fdd23092bcc602de
Rithvick <sairithvickgofficial@gmail.com>
2025-07-01 23:33:21 +0530

    Solved 2123A

---
+++ Code_Forces/Div3/2123/2123A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int n;
+        cin>>n;
+        if(n%4 == 0) cout<<"Bob\n";
+        else cout<<"Alice\n";
+    }
+}
